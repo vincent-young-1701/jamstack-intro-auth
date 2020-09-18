48d6c0e82a7161aa4d3da7df03a2e4c99fd4b082
Vincent Young <25315048+vincent-young-1701@users.noreply.github.com>
2020-09-17 23:26:55 -0700

    Initial

---
+++ src/components/route-base.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const RouteBase = () => <h1>All your base</h1>;
+
+export default RouteBase;
---
+++ src/components/route-secret.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const RouteSecret = () => <h1>Super Secret</h1>;
+
+export default RouteSecret;
